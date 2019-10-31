311f84da1f9e7e4150e47e61e040c24792035f06
a59566 <a59566@outlook.com>
2019-10-31 15:15:22 +0800

    add i18n settings

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,7 +2,7 @@
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 
 # Permitted locales available for the application
-I18n.available_locales = [:en, :zh-TW]
+I18n.available_locales = [:en, "zh-TW"]
 
 # Set default locale to something other than :en
-I18n.default_locale = :zh-TW
+I18n.default_locale = "zh-TW"
