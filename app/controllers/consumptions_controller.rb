a500d166da0a45f2bfb63e6360be753e49049c59
a59566 <a59566@outlook.com>
2019-11-05 16:26:50 +0800

    add pagination for consumption
    use kaminari gem

--- app/controllers/consumptions_controller.rb
+++ app/controllers/consumptions_controller.rb
@@ -2,7 +2,7 @@ class ConsumptionsController < ApplicationController
   before_action :set_consumption, only: [:edit, :update, :destroy]
 
   def index
-    @consumptions = Consumption.includes(:tag).all.order(id: :desc)
+    @consumptions = Consumption.includes(:tag).order(id: :desc).page(params[:page]).per(5)
   end
 
   def new
