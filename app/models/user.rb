19b53a1b99b29318ee4095a11d4f9c245731bd4b
a59566 <a59566@outlook.com>
2019-11-23 23:41:50 +0800

    add session limit
    use devise-security gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :timeoutable,
+         :session_limitable
 
   has_many :consumptions
   has_many :tags
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,7 +2,13 @@
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 
 # Permitted locales available for the application
-I18n.available_locales = [:en, "zh-TW"]
+I18n.available_locales = [:en, :"zh-TW"]
 
 # Set default locale to something other than :en
-I18n.default_locale = "zh-TW"
+I18n.default_locale = :"zh-TW"
+
+# Set fallbacks
+require "i18n/backend/fallbacks"
+I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+
+I18n.fallbacks.map(:"zh-TW" => :en)
