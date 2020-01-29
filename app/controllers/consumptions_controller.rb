1fcd03f1e378ce0760ac1fcfabcd371e374317de
a59566 <a59566@outlook.com>
2020-01-29 09:39:21 +0800

    add recent consumptions stacked bar chart

--- app/controllers/consumptions_controller.rb
+++ app/controllers/consumptions_controller.rb
@@ -2,8 +2,10 @@ class ConsumptionsController < ApplicationController
   before_action :set_consumption, only: [:edit, :update, :destroy]
 
   def index
-    @recent_5_days_consumptions = current_user.consumptions.where(:date => (Date.today-5)..(Date.today-1)).\
-                                    group(:date).order(date: :desc).select(:date, 'SUM(amount) as sum_amount')
+    @recent_5_days_consumptions = current_user.consumptions.joins(:tag).includes(:tag).where(:date => (Date.today-5)..(Date.today-1)).\
+                                    order(:date, :display_order)
+
+    @recent_5_days_consumptions = build_chart_data(@recent_5_days_consumptions)
 
     @this_month_amount = current_user.consumptions.this_month.sum(:amount)
 
@@ -53,5 +55,22 @@ class ConsumptionsController < ApplicationController
 
     def set_consumption
       @consumption = current_user.consumptions.find(params[:id])
+    end
+
+    def build_chart_data(sources)
+      result = []
+      result_element = {}
+      sources.group_by{|x| x.tag.name}.each do |key, value|
+        result_element = {}
+        result_element[:name] = key
+
+        data_array = []
+        value.each do |source|
+          data_array.push([source.date, source.amount])
+        end
+        result_element[:data] = data_array
+        result.push result_element
+      end
+      result
     end
 end
