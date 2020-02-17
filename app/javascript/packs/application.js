1657d987b306f1d66f607470a6a2bcf73bef1da4
a59566 <a59566@outlook.com>
2020-02-17 22:41:43 +0800

    use data-confirm-modal to customize confirm() api

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels");
 require("chartkick");
 require("chart.js");
 require("bootstrap");
+require("data-confirm-modal");
 require("../src/application.scss");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,15 +20,22 @@ require("../src/application.scss");
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+dataConfirmModal.setDefaults({
+    title: '確認視窗',
+    commit: '確認',
+    cancel: '取消',
+    cancelClass: 'btn-secondary'
+});
 
-// delete table row when destroy action succeed
 document.addEventListener('turbolinks:load', function () {
+    // delete table row when destroy action succeed
     document.querySelectorAll('.delete').forEach(function (a) {
         a.addEventListener('ajax:success', function () {
             const tr = a.parentNode.parentNode;
             tr.style.display = 'none';
         })
     });
+
 
     document.querySelectorAll('form[data-remote="true"]').forEach(function (form) {
         form.addEventListener('ajax:error', function (event) {
