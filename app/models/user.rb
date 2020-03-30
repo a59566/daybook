7ce1e1cc301149015586461167ac914cf1c19a68
a59566 <a59566@outlook.com>
2020-03-31 06:35:02 +0800

    edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable,
-         :session_limitable
-
   has_many :consumptions, dependent: :delete_all
   has_many :tags, dependent: :delete_all
+
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
 end
