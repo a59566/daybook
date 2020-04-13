6ceae95985bcf0e5deefdd52774640d2c241cf01
a59566 <a59566@outlook.com>
2020-04-14 03:29:02 +0800

    remove SQL in migration file

--- db/migrate/20191120172403_add_not_null_user_to_consumptions.rb
+++ db/migrate/20191120172403_add_not_null_user_to_consumptions.rb
@@ -1,6 +1,5 @@
 class AddNotNullUserToConsumptions < ActiveRecord::Migration[6.0]
   def up
-    execute 'UPDATE consumptions SET user_id = 1'
     change_column_null :consumptions, :user_id, false
   end
 
